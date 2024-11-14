f0172765a3adf09a9051aa69e3c29e5a167fe1f4
LewisLegstrong <61909038+LewisLegstrong@users.noreply.github.com>
2024-11-14 14:49:40 +0000

    Add files via upload

---
+++ Projeto/inc/date_and_time.hpp
@@ -0,0 +1,26 @@
+#ifndef TIME_AND_DATE_HPP
+#define TIME_AND_DATE_HPP
+
+#include <iostream>
+
+class Date {
+private: 
+	int day;
+	int month;
+	int year;
+	
+public:
+
+};
+
+
+class Time {
+private:
+	int hour;
+	int minute;
+
+public:
+
+};
+
+#endif // TIME_AND_DATE_HPP
