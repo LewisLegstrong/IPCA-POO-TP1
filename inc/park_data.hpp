1f2a86311ded33d552ecf7bd1de68d3591e8e99c
Paulo Palhares <a23080@alunos.ipca.pt>
2024-11-18 16:44:10 +0000

    class Registration/vehicle

--- inc/park_data.hpp
+++ inc/park_data.hpp
@@ -6,7 +6,17 @@
 
 #include "time.hpp"
 
-class Vehicle {
+class Registration {
+private:
+	Time	*entry;
+	Time  	*exit;
+
+public:
+	Registration (Time &in);
+	void setExitTime(Time &out);
+};
+
+class Vehicle:Registration {
 private:
 	std::string license_plate;
 	std::string type;
@@ -15,17 +25,4 @@ public:
 	Vehicle(std::string lp, std::string t);
 };
 
-class IORegistry {
-private:
-	Vehicle *information;
-	Time	*entry;
-	Time  	*exit;
-
-public:
-	IORegistry (Vehicle &v, Time &in);
-	void setExitTime(Time &out);
-};
-
-
-
-#endif 		// _PARK_DATA_
+#endif	// PARK_DATA
