050c82b37af9bdb41d609fa512e0a329eb8f1cdf
Luis Manuel Duarte Barrão <luis.barrao@ceiia.com>
2024-11-25 22:25:09 +0000

    pushing updated main

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
     Vehicle myVehicle(licensePlate);
 
     // Create a Time object with the entry time (hours and minutes)
-	Time entryTime(8, 30, 0);
+	Time entryTime(18, 30, 0);
     myPark.newEntry(myVehicle, entryTime);
 
 //////////////////////////////////////////////////////////////////////
@@ -39,7 +39,7 @@ int main() {
 
 
     //Remove a vehicle from the park
-    Time exitTime(11, 45, 0);
+    Time exitTime(20, 30, 0);
     myPark.removeEntry(licensePlate, exitTime);
 
     // List all vehicles in the park again
@@ -60,12 +60,12 @@ int main() {
     // readFile("carro.txt");
 
 
-    // Time spent in the park
-    // Time actualHour (17, 30, 0);
-    // int timeSpent = myPark.getTimeSpentInPark(licensePlate1, actualHour);
-    // if (timeSpent != -1) {
-    //     std::cout << "Vehicle with license plate " << licensePlate << " has been in the park for " << timeSpent << " minutes." << std::endl;
-    // }
+    //Time spent in the park
+    Time actualHour (17, 30, 0);
+    int timeSpent = myPark.getTimeSpentInPark(licensePlate1, actualHour);
+    if (timeSpent != -1) {
+        std::cout << "Vehicle with license plate " << licensePlate << " has been in the park for " << timeSpent << " minutes." << std::endl;
+    }
 
 	return 0;
 }
