dd5f29ac4e14c533c86a927a7b7d3fc0f6c1f56d
Luis Manuel Duarte Barrão <luis.barrao@ceiia.com>
2024-12-23 10:21:47 +0000

    New class to work on

---
+++ inc/monthlyParkSubscription.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "vehicle_reg.hpp"
+
+
+
+class monthlyParkSubscription {
+public:
+	monthlyParkSubscription();
+
+private:
+	int 		nif;
+	bool 		isActive;
+	Vehicle 	vehicle;
+	std::string clientName;
+
+public:
+
+	 
+
+
+
+};
