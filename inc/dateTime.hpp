7e61cec1efc21da8eec947c4a2f8a10d4161a551
Luis Duarte <luis.duarte@outlook.pt>
2024-12-23 19:09:07 +0000

    dateTime are a work in progress

--- inc/dateTime.hpp
+++ inc/dateTime.hpp
@@ -7,10 +7,13 @@
 #include "date.hpp"
 #include "time.hpp"
 
+
 class dateTime : public Date, public Time {	
 public: 
 	/// @brief Constructor for a class derived from Date and Time. meant to simplify the verification process
 	dateTime(int registerDay, int registerMonth, int registerYear, int registerHour, int registerMinute, int registerSeconds);
+
+	float operator- (const dateTime &toSubtract);
 };
 
 
