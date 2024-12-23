4f0f5401c10b84f89585cf18cb871199b55a9dd7
Luis Duarte <luis.duarte@outlook.pt>
2024-12-23 16:32:09 +0000

    Added a function name to the class

--- inc/monthlyParkSubscription.hpp
+++ inc/monthlyParkSubscription.hpp
@@ -6,17 +6,20 @@
 
 class monthlyParkSubscription {
 public:
-	monthlyParkSubscription(int nifRegister, Vehicle *vehicleRegister);
+	monthlyParkSubscription(int nifRegister, Vehicle *vehicleRegister, std::string name);
 
 private:
 	int 		nif;
 	bool 		isActive;
 	Vehicle 	*registeredVehicle;
+	std::string customerName;
 
 public:
-	bool 	getIsActive();
-	 
+	/// @brief Activates the monthly subscription
+	void activateSubscription();
 
-
+	/// @brief Returns if the monthly subscription is active
+	/// @return true if the subscription is active, false otherwise
+	bool getIsActive();
 
 };
