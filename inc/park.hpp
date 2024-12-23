c091ca3c1c9d91c208a8cc4c1666577e678b3684
Luis Duarte <luis.duarte@outlook.pt>
2024-12-23 17:06:58 +0000

    Added briefs Added GestPark class

--- inc/park.hpp
+++ inc/park.hpp
@@ -26,47 +26,79 @@ private:
 
 public:
 	/// @brief list the vehicles parked in the park
-	void listVehicles();
+	void listVehicles( );
 
 	/// @brief register a new vehicle in the park, saves information about time of entry and vehicle
 	/// @param v 
 	/// @param in 
-	void newEntry (Vehicle &v, Time &in);
+	void newEntry ( Vehicle &v, Time &in );
 
 	/// @brief remove a vehicle from the park, saves information about time of exit and vehicle and removes the registry from the parkedVehicles vector
 	/// @param licensePlate
 	/// @param out
-	void removeEntry(const std::string &licensePlate, Time &out);
+	void removeEntry ( const std::string &licensePlate, Time &out );
 	
 	/// @brief sum the value paid by the vehicle to the accumulated value of the park
 	/// @param pricePaid
-	void sumAccValue( float pricePaid );
+	void sumAccValue ( float pricePaid );
 	
 	/// @brief consult the history of a vehicle in the park
 	/// @param licensePlate 
-	void consultHistoric( std::string licensePlate );
+	void consultHistoric ( std::string licensePlate );
 
 	/// @brief Verifies the number of empty spots in the park
 	/// @return 
-	int getEmptySpots();
+	int getEmptySpots ( );
     
 	/// @brief Verifies the number of occupied spots in the park
-	int getOccupiedSpots();
+	int getOccupiedSpots ( );
 	
 	/// @brief Verifies the time spent in the park by a vehicle
 	/// @param licensePlate 
 	/// @param current 
-	/// @return 
-	int getTimeSpentInPark(const std::string &licensePlate, Time &current) const;
+	/// @return time in minutes
+	int getTimeSpentInPark ( const std::string &licensePlate, Time &current ) const;
 
 	/// @brief Verifies if a vehicle is parked in the park
-	bool isParked(std::string &licensePlate);
+	bool isParked ( std::string &licensePlate );
 
 	/// @brief obtain the accumulated value of the park 
-	float getAccValue();
+	float getAccValue ( );
 	
 	/// @brief Calculate a simulated price to receive from a vehicle at a simulated exit time
-	float getSimulatedPriceToReceive( Time &simulatedTime );
+	/// @return The price to be paid by the vehicle
+	float getSimulatedPriceToReceive ( Time &simulatedTime );
 } ;
 
+
+class GestPark {
+public:
+	/// @brief GestPark constructor, meant to initialize a park retaining it's location and capacity
+	GestPark( );
+	/// @brief GestPark destructor
+	~GestPark( );
+
+private:
+	std::vector<Park> avaialbleParks;
+
+public:
+	/// @brief Insert a new park in the city
+	void insertParkInCity ( );
+
+	/// @brief List all parks in given city
+	void listParksInCity ( std::string city );
+
+	// Register a new vehicle in the park is included in park.hpp
+
+	/// @brief  finds where a vehicle is parked
+	/// @return The City where the car is parked
+	std::string findWhereIsParked ( std::string licensePlate );
+
+	// Return the history of a vehicle in a park is included in park.hpp
+	
+	/// @brief Obtain payments received by a client
+	/// @return The months paid by the client
+	std::string paymentsReceivedByClient ( int nif );
+
+};
 #endif 		// _PARK_
