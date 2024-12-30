b7e70536d845ba2ef88b8850009dc1a09b3759d6
Luis Duarte <luis.duarte@outlook.pt>
2024-12-30 19:22:27 +0000

    Added verification for days from a map. Commented at this time

--- src/date.cpp
+++ src/date.cpp
@@ -3,19 +3,23 @@
 Date::Date(int d, int m, int y) {
 	this->year = y;
 	this->month = m;
-	if ( d > monthDays[ m ] ) {
-		throw std::invalid_argument("Invalid day");
-	}
-	else {
-		this->day = d;
-	}
+	this->day = d;
+
+	std::cout << "Month: " << m << "; Day: " << d << std::endl;
+
+	// if ( d > monthDays[m]  || d < 1 ) {
+	// 	throw std::invalid_argument("Invalid day");
+	// }
+	// else {
+	// 	this->day = d;
+	// }
 }
 
 int Date::getDay() {
 	return this->day;
 }
 
-int  Date::getMonth() {
+int Date::getMonth() {
 	return this->month;
 }
 
