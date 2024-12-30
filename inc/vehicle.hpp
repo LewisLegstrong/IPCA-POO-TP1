d4bd7231e472b7436f2ac287c9770784d5ac48ad
Luis Duarte <luis.duarte@outlook.pt>
2024-12-30 19:22:08 +0000

    Minor changes

--- inc/vehicle.hpp
+++ inc/vehicle.hpp
@@ -4,15 +4,16 @@
 #include <string>
 #include <iostream>
 
+
 /// @brief Class to define the vehicle
 class Vehicle {
 public:
 	/// @brief 		Constructor
 	/// @param lp -> License plate of the vehicle 
-	Vehicle(std::string lp);
-
+	Vehicle( std::string lp );
+	
 private:
-	std::string license_plate;
+	std::string 	license_plate;
 
 public:
 	/// @brief 		Returns the license plate of the vehicle registered in the object Vehicle
--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1,9 +1,7 @@
 #include "vehicle.hpp"
 
 
-Vehicle::Vehicle(std::string lp) {
-	this->license_plate = lp;
-}
+Vehicle::Vehicle( std::string lp) : license_plate( lp ) {}
 
 std::string Vehicle::getLicensePlate() {
 	return this->license_plate;
